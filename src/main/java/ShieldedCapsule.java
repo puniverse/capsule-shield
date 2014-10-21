3fb44e9501e5ec3b84c974d89462e212621a145d
pron <ron.pressler@gmail.com>
2014-10-22 00:57:40 +0300

    initial commit

---
+++ src/main/java/ShieldedCapsule.java
@@ -0,0 +1,19 @@
+
+import java.nio.file.Path;
+
+/*
+ * Copyright (c) 2014, Parallel Universe Software Co. and Contributors. All rights reserved.
+ * 
+ * This program and the accompanying materials are licensed under the terms 
+ * of the Eclipse Public License v1.0, available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ */
+/**
+ *
+ * @author pron
+ */
+public class ShieldedCapsule extends Capsule {
+    public ShieldedCapsule(Path jarFile, Path cacheDir) {
+        super(jarFile, cacheDir);
+    }
+}
