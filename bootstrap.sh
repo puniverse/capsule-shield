1e5079ad71699c647d3290d6adc6304c3cc16808
pron <ron.pressler@gmail.com>
2014-10-22 02:52:36 +0300

    Added Vagrantfile

---
+++ bootstrap.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# add-apt-repository ppa:webupd8team/java
+apt-get update
+apt-get upgrade -y 
+apt-get install -y lxc uidmap
+apt-get install -y openjdk-7-jre
+# apt-get install oracle-java8-installer
